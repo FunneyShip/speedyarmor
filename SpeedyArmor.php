c09f48fa7f44d363e4f51f57f2dd7f6c77cb7e67
hamachichi <hamachichi@hotmail.com>
2014-06-07 01:53:57 -0400

    Update SpeedyArmor.php

--- SpeedyArmor.php
+++ SpeedyArmor.php
@@ -1,16 +1,13 @@
 <?php
 /*
-__Pocketmine Plugin__
-name=SpeedyAromor
-description=the speedest way to change armor
-version=0.1
-Author=hamachichi
-class=speed
-apiversion=11,12,13,
+__PocketMine Plugin__
+class=SpeedyArmor
+name=SpeedyArmor
+author=hama
+version=1
+apiversion=12,13
 */
-
-class speed implements Plugin
-{
+class SpeedyArmor implements Plugin{
     private $api;
 
     public function __construct(ServerAPI $api, $server = false)
@@ -45,27 +42,65 @@ class speed implements Plugin
       $player->setArmor(1,$this->getArmor(311));
       $player->setArmor(2,$this->getArmor(312));
       $player->setArmor(3,$this->getArmor(313));
-                }
-                break;
-                     }
                 
-                  }
+                }
+                  break;
+                  
+                  
+                  case "player.equipment.change":
+
+                if($data["266"]->getID() == $this->CONFIG["yellow"])
+                {
+      $player->setArmor(0,$this->getArmor(314));
+      $player->setArmor(1,$this->getArmor(315));
+      $player->setArmor(2,$this->getArmor(316));
+      $player->setArmor(3,$this->getArmor(317));
+                }
+                  break;
+                 
+
+                  case "player.equipment.change":
+
+                if($data["265"]->getID() == $this->CONFIG["gray"])
+                {
+      $player->setArmor(0,$this->getArmor(306));
+      $player->setArmor(1,$this->getArmor(307));
+      $player->setArmor(2,$this->getArmor(308));
+      $player->setArmor(3,$this->getArmor(309));
+                }
+            
+                  break;
+                  
+                   case "player.equipment.change":
+
+                if($data["334"]->getID() == $this->CONFIG["brown"])
+                {
+      $player->setArmor(0,$this->getArmor(298));
+      $player->setArmor(1,$this->getArmor(299));
+      $player->setArmor(2,$this->getArmor(300));
+      $player->setArmor(3,$this->getArmor(301));
+                }
+                
+                  break;
+                  
+                   case "player.equipment.change":
+
+                if($data["331"]->getID() == $this->CONFIG["red"])
+                {
+      $player->setArmor(0,$this->getArmor(310));
+      $player->setArmor(1,$this->getArmor(307));
+      $player->setArmor(2,$this->getArmor(316));
+      $player->setArmor(3,$this->getArmor(301));
+                }
+                
+                  break;
+                  
+        }
+    }
+                  
           public function __destruct(){
     }
                 
 }          
                 
 ?>
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
